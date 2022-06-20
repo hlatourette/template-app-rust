289bc003e5f2796147fbe841470b9606b623b96d
Hunter LaTourette <hlatourette@gmail.com>
2022-06-19 23:29:52 -0400

    refactor (*): use module for utility lib

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use std::io;
 
+mod utility;
+
 fn main() {
     let mut guess = String::new();
-
     println!("What would you like to know?");
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("{}", meaning_of_life_the_universe_and_everything(&guess));
-}
-
-// TODO: move into example module
-fn meaning_of_life_the_universe_and_everything(_question: &String) -> i32 { // parameter prefixed to suppress warning
-    42
+    println!("{}", utility::deep_thought::meaning_of_life_the_universe_and_everything(&guess));
 }
